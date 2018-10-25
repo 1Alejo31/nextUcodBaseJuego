4f51cc3822e7fbd21ca3f6dc04ced89ff3c72884
1Alejo31 <31145289+1Alejo31@users.noreply.github.com>
2018-10-25 04:47:28 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+//Iniciar juego
+
+
+//Cambio de color indefinidamente
+    setInterval(function(){
+        $(".main-titulo").css("color", "#FFFFFF");
+        
+    }, 1000);   
+    setInterval(function(){
+        $(".main-titulo").css("color", "#EAED26");
+        
+    }, 1175);   
+
+    
+});
+
+    
