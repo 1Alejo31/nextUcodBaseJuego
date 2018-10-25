a18381557d7de62900518772be85dec4bacb89ae
1Alejo31 <31145289+1Alejo31@users.noreply.github.com>
2018-10-25 04:50:54 -0500

    crear las imagenes aleatoreas
    llenando el recuadro

--- js/app.js
+++ js/app.js
@@ -12,7 +12,23 @@ $(document).ready(function(){
         
     }, 1175);   
 
+    //Imagenes aleatoreas y llenar el cuadro
+
+    for(var c = 1; c <8; c++){
+        for(var j = 1; j < 7; j++ ){
+            if($(".col-"+j).children("img:nth-child("+j+")").html()==null);
+            {
+                imagen = Math.floor(Math.random()*4)+1;
+                imagenes = "image/"+imagen+".png";
+                $(".col-"+c).prepend("<img id='theImg' src="+imagenes+" width='99%'> ");
+            }
+        }
+    }
+    
     
 });
+
+
+}
 
     
